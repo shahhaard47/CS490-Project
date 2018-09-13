3524456b32b047e935ffe3ff550cb8cc43fe45d8
TheMightyE <sk2283@njit.edu>
2018-09-12 21:00:03 -0400

    Connectivity works with middle end. Minor UI changes.

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,22 +1,21 @@
 function authenticateLogin() {
     /* Display the loading icon */
-    // document.getElementById('loading').style.visibility = 'visible';
-
+    document.getElementById('loading').style.visibility = 'visible';
+    document.getElementById('submit-btn').setAttribute("value", "Signing In ...");
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
             /* Hide the loading icon */
-            // document.getElementById('loading').style.visibility = 'hidden';
-            document.getElementById('loading').innerHTML = xhr.responseText;
+            document.getElementById('loading').style.visibility = 'hidden';
+            document.getElementById('response').innerHTML = xhr.responseText;
+            document.getElementById('submit-btn').setAttribute("value", "Sign In");
         }
     };
-    xhr.open('POST', '/~hks32/index.php', true);
-    xhr.setRequestHeader("Content-type", "application/x-www-for-urlencoded");
+    xhr.open('POST', '/~hks32/CS490-Project/assets/middle/index.php', true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     let user = document.getElementById("user").value;
     let pass = document.getElementById("pass").value;
     xhr.send("user=" + user + "&pass=" + pass);
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-
-});
+// document.addEventListener('DOMContentLoaded', function () {}
