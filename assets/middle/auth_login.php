f90dbf8683a1be8d7d7250ac780e5ae38afc3546
shahhaard47 <hks32@njit.edu>
2018-10-10 22:12:14 -0400

    added auth_login.php three days ago.

--- assets/middle/auth_login.php
+++ assets/middle/auth_login.php
@@ -1,14 +1,14 @@
-<?php  
+<?php
 
 $jsondata = file_get_contents('php://input'); #check contents and send to debbie
 
 $data = json_decode($jsondata, true);
 
 if($data["user"] && $data["pass"])
-{	
+{
 	//*	curl send to debbie
 	$curl_opts = array(CURLOPT_POST => 1,
-		   	   CURLOPT_URL => 'https://web.njit.edu/~ds547/CS490-Project/assets/back/back.php',
+		   	   CURLOPT_URL => 'https://web.njit.edu/~ds547/CS490-Project/assets/back/back_login.php',
 		   	   CURLOPT_POSTFIELDS => $jsondata,
 			   CURLOPT_RETURNTRANSFER => 1);
 	$ch = curl_init();
